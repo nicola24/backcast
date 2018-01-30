fbecd2847962680364c90debcc2c43f5b9ed9257
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-29 20:18:56 -0800

    removed app view message

--- src/views/app.js
+++ src/views/app.js
@@ -3,15 +3,23 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
+    console.log('please');
     this.videos = new Videos();
+    this.render();
   },
 
 
   render: function() {
+    console.log('---------------------------', SearchView);
+    new SearchView().render();
+    new VideoListView().render();
+    new VideoListEntryView().render();
+    new VideoPlayerView().render();
     this.$el.html(this.template());
-    return this;
+    return this; 
   },
 
   template: templateURL('src/templates/app.html')
 
 });
+  
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -2,7 +2,7 @@ var VideoListEntryView = Backbone.View.extend({
 
 
   render: function() {
-    this.$el.html(this.template(this.model.attributes));
+    this.$el.html(this.template());
     return this;
   },
 
