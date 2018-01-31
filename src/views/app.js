10a0ce90b2bf62299d7daff4dc4ede35a6253744
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-30 21:23:08 -0800

    search function works with dynamic data

--- src/views/app.js
+++ src/views/app.js
@@ -2,29 +2,38 @@ var AppView = Backbone.View.extend({
 
   el: '#app',
 
-  collection: new Videos(window.exampleVideoData),
+  // collection: new Videos(window.exampleVideoData),
 
   initialize: function() {
     this.videos = new Videos();
+    this.listenTo(this.videos, 'sync', this.firstVid);
+    this.videos.search();
     this.render();
   },
+
+  firstVid: function() {
+    if (this.videos.length > 0) {
+      this.videos.at(0).select();
+    }
+  },
+
 
   render: function() {
     this.$el.html(this.template());
 
     new VideoPlayerView({
-      collection: this.collection,
+      collection: this.videos,
       el: this.$('.player'),
-      model: this.collection.models[0]
+      model: this.videos.at(0)
     }).render();
 
     new VideoListView({
-      collection: this.collection,
+      collection: this.videos,
       el: this.$('.list')
     }).render();
 
     new SearchView({
-      collection: this.collection,
+      collection: this.videos,
       el: this.$('.search')
     }).render();
     return this; 
--- src/views/search.js
+++ src/views/search.js
@@ -1,13 +1,25 @@
 var SearchView = Backbone.View.extend({
 
 
+  events: {
+    'click .btn' : 'doSearch',
+    'keyup input' : 'keyHit'
+    //keyboard enter 
+  },
 
-
-
+  keyHit: function(e) {
+    if (e.keyCode === 13) {
+      this.doSearch();
+    }
+  },
 
   doSearch: function() {
-    
-
+    var searched = $('input').val();
+    if (searched) {
+      this.collection.search(searched);
+    } else {
+      $('input').val();  
+    }
 
   },
 
